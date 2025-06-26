f91163e3c093f8308d0597a36824a81f8840a93f
Mitchell Blankenship <mitblan@gmail.com>
2025-06-25 21:30:02 -0400

    Start product listing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+
 const Homepage = () => {
-  return ( <>Prostore</> );
+	return (
+		<>
+			<ProductList data={sampleData.products} title='Newest Stuff' limit={4} />
+		</>
+	)
 }
- 
-export default Homepage;
+
+export default Homepage
