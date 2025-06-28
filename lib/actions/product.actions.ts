35e343239693d52efcdce8be525abcb80e24a8aa
Mitchell Blankenship <mitblan@gmail.com>
2025-06-28 18:18:15 -0400

    jumped through a bunch of hoops to get things working since I'm not using neon

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,10 @@
 'use server'
+import { prisma } from '@/db/prisma'
 import { LATEST_PRODUCTS_LIMIT } from "../constants"
-import { PrismaClient } from "../generated/prisma"
 import { convertToPlainObject } from "../utils"
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient()
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
