07fa4df55db5c1d98cdcf390323152b02828b0cd
Mitchell Blankenship <mitblan@gmail.com>
2025-07-24 19:30:27 -0400

    Users can now sign in

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from 'bcrypt-ts-edge';
+
 const sampleData = {
+  users: [
+    {
+      name: 'John',
+      email: 'admin@test.com',
+      password: hashSync('123456789', 10),
+      role: 'admin'
+    },
+    {
+      name: 'Jane',
+      email: 'user@test.com',
+      password: hashSync('123456789', 10),
+      role: 'user'
+    },
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
